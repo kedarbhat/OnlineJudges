74aa509a0f511ec238b263adaf85bc042eebbee0
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-01 15:50:44 -0600

    Time: 108 ms (32.75%), Space: 46.7 MB (15.93%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+      std::unordered_set<int> nums_seen;
+      auto max_number = 0;
+      for (auto i : nums) {
+          if (i > 0) {
+              nums_seen.emplace(i);
+              max_number = std::max(max_number, i);
+          }
+      }
+      for (auto i = 1; i < max_number; ++i) {
+          if (nums_seen.find(i) == nums_seen.cend()) {
+            return i;
+          }
+      }
+      return max_number+1;
+    }
+};
