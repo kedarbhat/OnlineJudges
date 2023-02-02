67878c662990b0b6256808952ae76339deeeff34
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-02 13:48:09 -0600

    Time: 51 ms (35.2%), Space: 71.4 MB (53.89%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        std::unique_ptr<ListNode> dummyHead = std::make_unique<ListNode>(0);
+        ListNode* curr = dummyHead.get();
+        auto carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            auto x = l1 != nullptr ? l1->val : 0;
+            auto y = l2 != nullptr ? l2->val : 0;
+            auto sum = carry + x + y;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+
+            // update ListNode pointers
+            curr = curr->next;
+            l1 = l1 != nullptr ? l1->next : nullptr;
+            l2 = l2 != nullptr ? l2->next : nullptr;
+        }
+        return dummyHead->next;
+    }
+};
