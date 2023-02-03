a75007a4e3876d05872697a455a1606955862ae8
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-03 15:02:16 -0600

    Time: 111 ms (62.21%), Space: 7 MB (5.92%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.cpp
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.cpp
@@ -1,10 +1,11 @@
 class ZeroEvenOdd {
 private:
-    std::condition_variable theCv;
-    std::mutex theMutex;
-    bool thePrintZero{true};
-    int theCounter{1};
+    std::atomic_flag thePrintZero = ATOMIC_FLAG_INIT;
+    std::atomic_flag thePrintEven = ATOMIC_FLAG_INIT;
+    std::atomic_flag thePrintOdd = ATOMIC_FLAG_INIT;
+    int theCounter{0};
     const int theN;
+
 public:
     ZeroEvenOdd(int n) : theN{n} 
     {
@@ -12,45 +13,42 @@ public:
 
     // printNumber(x) outputs "x", where x is an integer.
     void zero(function<void(int)> printNumber) {
-        while (true) {
-            std::unique_lock<std::mutex> myLock{theMutex};
-            theCv.wait(myLock, [&]() { return thePrintZero || theCounter > theN; });
-            if (theCounter > theN) {
-                break;
+        while (theCounter <= theN) {
+            if (!thePrintZero.test_and_set(std::memory_order_acq_rel)) {
+                if (theCounter < theN) {
+                    printNumber(0);
+                }
+                ++theCounter;
+                if ((theCounter & 1) == 1) {
+                    thePrintOdd.clear(std::memory_order_release);
+                } else {
+                    thePrintEven.clear(std::memory_order_release);
+                }
+            } else {
+                std::this_thread::yield();
             }
-            printNumber(0);  
-            thePrintZero = false;
-            theCv.notify_all();
         }
     }
 
     void even(function<void(int)> printNumber) {
-        while (true) {
-            std::unique_lock<std::mutex> myLock{theMutex};
-            theCv.wait(myLock,
-                       [&]() { return (!thePrintZero && theCounter % 2 == 0) || theCounter > theN; });
-            if (theCounter > theN) {
-                break;
+        while (theCounter <= theN) {
+            if (theCounter % 2 == 0 && !thePrintEven.test_and_set(std::memory_order_acq_rel)) {
+                printNumber(theCounter);
+                thePrintZero.clear(std::memory_order_release);
+            } else {
+                std::this_thread::yield();
             }
-            printNumber(theCounter);    
-            ++theCounter;
-            thePrintZero = true;
-            theCv.notify_all();
         }
     }
 
     void odd(function<void(int)> printNumber) {
-        while (true) {
-            std::unique_lock<std::mutex> myLock{theMutex};
-            theCv.wait(myLock,
-                       [&]() { return (!thePrintZero && theCounter % 2 == 1) || theCounter > theN; });
-            if (theCounter > theN) {
-                break;
+        while (theCounter <= theN) {
+            if (theCounter % 2 == 1 && !thePrintOdd.test_and_set(std::memory_order_acq_rel)) {
+                printNumber(theCounter);
+                thePrintZero.clear(std::memory_order_release);
+            } else {
+                std::this_thread::yield();
             }
-            printNumber(theCounter);    
-            ++theCounter;
-            thePrintZero = true;
-            theCv.notify_all();
         }
     }
 };
