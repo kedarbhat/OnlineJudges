b0f1456718c61a822654ea5b50e31e2bdedad385
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-03 23:23:33 -0600

    Time: 376 ms (29.24%), Space: 7.2 MB (65.36%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,29 +1,33 @@
 class Foo {
-    std::atomic_flag printSecondFlag = ATOMIC_FLAG_INIT;
-    std::atomic_flag printThirdFlag = ATOMIC_FLAG_INIT;
+   std::mutex theMutex;
+   std::condition_variable theCv;
+   std::atomic_int theCounter{1};
 
 public:
-    Foo() {
-        printSecondFlag.test_and_set();
-        printThirdFlag.test_and_set();
-    }
     void first(function<void()> printFirst) {
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
-        printSecondFlag.clear();
-
+        theCounter.store(2, std::memory_order_release);
+        theCv.notify_all();
     }
 
     void second(function<void()> printSecond) {
         // printSecond() outputs "second". Do not change or remove this line.
-        while (printSecondFlag.test_and_set()) {}
+        {
+            std::unique_lock myLock{theMutex};
+            theCv.wait(myLock, [&theCounter = theCounter]() { return theCounter.load(std::memory_order_acquire) == 2; });
+        }
         printSecond();
-        printThirdFlag.clear();
+        theCounter.store(3, std::memory_order_release);
+        theCv.notify_all();
     }
 
     void third(function<void()> printThird) {
         // printThird() outputs "third". Do not change or remove this line.
-        while (printThirdFlag.test_and_set()) {}
+        {
+            std::unique_lock myLock{theMutex};
+            theCv.wait(myLock, [&theCounter = theCounter]() { return theCounter.load(std::memory_order_acquire) == 3; });
+        }
         printThird();
     }
 };
