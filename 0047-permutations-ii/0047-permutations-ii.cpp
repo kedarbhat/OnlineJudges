f4e2b0366c0d76b3838c76b3ea04b73600b31d86
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-07 07:19:03 -0600

    Time: 5 ms (86.5%), Space: 9 MB (68.61%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -6,15 +6,15 @@ class Solution {
             theResult.emplace_back(vector<int>(path));
             return;
         }
-        for (auto& [num, count] : theCounter) {
-            if (theCounter[num] == 0) {
+        for (auto& [myNum, myCount] : theCounter) {
+            if (myCount == 0) {
                 continue;
             }
-            --count;
-            path.push_back(num);
+            --myCount;
+            path.push_back(myNum);
             dfs(nums, startIdx+1, path);
             path.pop_back();
-            ++count;
+            ++myCount;
         }
     }
 public:
