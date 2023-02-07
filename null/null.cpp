c17ab55f6f93ecbd46d1e2ac17e6d1602558e31d
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-07 00:22:10 -0600

    Time: 7 ms (79.76%), Space: 13.7 MB (98.16%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,38 +1,21 @@
-class Solution {
-   void dfs(vector<vector<char>>& grid, int r, int c) {
-    const int numRows = grid.size();
-    const int numCols = grid[0].size();
 
-    grid[r][c] = '0';
-    if (r - 1 >= 0 && grid[r-1][c] == '1') {
-        dfs(grid, r - 1, c);
-    }
-    if (r + 1 < numRows && grid[r+1][c] == '1') {
-        dfs(grid, r + 1, c);
-    }
-    if (c - 1 >= 0 && grid[r][c-1] == '1') {
-         dfs(grid, r, c - 1);
-    }
-    if (c + 1 < numCols && grid[r][c+1] == '1') { 
-        dfs(grid, r, c + 1);
-    }
-  }
+class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.size() == 0) {
+    int maxProduct(vector<int>& nums) {
+        if (nums.empty()) {
             return 0;
         }
-        const int numRows = grid.size();
-        const int numCols = grid[0].size();
-        int numIslands = 0;
-        for (auto r = 0; r < numRows; ++r) {
-            for (auto c = 0; c < numCols; ++c) {
-                if (grid[r][c] == '1') {
-                    dfs(grid, r, c);
-                    ++numIslands;
-                }
-            }
+        auto minProduct = nums[0];
+        auto maxProduct = nums[0];
+        auto result = nums[0];
+        for (auto i = 1u; i < nums.size(); ++i) {
+            auto prevMaxProduct = maxProduct;
+            auto prevMinProduct = minProduct;
+            minProduct = std::min({nums[i], prevMaxProduct*nums[i], prevMinProduct*nums[i]});
+            maxProduct = std::max({nums[i], prevMaxProduct*nums[i], prevMinProduct*nums[i]});
+            result = std::max(maxProduct, result);
         }
-        return numIslands;
+        return result;
+        
     }
 };
