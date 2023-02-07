8800ad591cb7a53a4e189f75d944bebb56abef09
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-06 20:01:43 -0600

    Time: 22 ms (61.59%), Space: 11.8 MB (89.29%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,18 +1,17 @@
-class Solution {
+class Solution {      
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        auto carry = 1;
-        for (int i = digits.size()-1; carry == 1 && i >= 0; --i) {
-            digits[i] += carry;
-            if (digits[i] < 10) {
-                carry = 0;
-            } else {
-                digits[i] = 0;
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](const auto& lhs, const auto& rhs) {
+            return lhs[0] < rhs[0];
+        });
+        std::priority_queue<int, vector<int>, std::greater<>> heap;
+        heap.push(intervals[0][1]);
+        for (auto i = 1; i < intervals.size(); ++i) {
+            if (heap.top() <= intervals[i][0]) {
+                heap.pop();
             }
+            heap.push(intervals[i][1]);
         }
-        if (carry == 1) {
-            digits.insert(digits.begin(), 1);
-        }
-        return digits;
+        return heap.size();
     }
 };
