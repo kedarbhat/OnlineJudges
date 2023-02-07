bd408ccd5bad80c772974d4ab0eb34eda0a9346c
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-06 19:56:04 -0600

    Time: 0 ms (100%), Space: 8.6 MB (98.93%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,36 +1,18 @@
-class RandomizedCollection {
-    std::unordered_map<int, std::unordered_set<int>> theMap;
-    std::vector<int> theData;
+class Solution {
 public:
-    bool insert(const int val) noexcept {
-        theData.emplace_back(val);
-        theMap[val].emplace(theData.size()-1);
-        return theMap[val].size() == 1;
-    }
-
-    bool remove(const int val) noexcept {
-        if (theMap.find(val) == theMap.cend() || theMap[val].empty()) {
-            return false;
-        } 
-        auto prevLastElement = theData.back();
-        auto removeIdx = *theMap[val].cbegin();
-        theMap[val].erase(theMap[val].cbegin());
-        theMap[prevLastElement].emplace(removeIdx);
-        theMap[prevLastElement].erase(theData.size()-1);
-        theData[removeIdx] = prevLastElement;
-        theData.pop_back();
-        return true;
-    }
-    
-    int getRandom() noexcept {
-        return theData[std::rand() % theData.size()];
+    vector<int> plusOne(vector<int>& digits) {
+        auto carry = 1;
+        for (int i = digits.size()-1; carry == 1 && i >= 0; --i) {
+            digits[i] += carry;
+            if (digits[i] < 10) {
+                carry = 0;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        if (carry == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
     }
 };
-
-/**
- * Your RandomizedCollection object will be instantiated and called as such:
- * RandomizedCollection* obj = new RandomizedCollection();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
