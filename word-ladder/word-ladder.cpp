238253b7aefd568f433b7718ef7c04f17ff64cbc
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-06 23:23:12 -0600

    Time: 152 ms, Memory: 51 MB - LeetHub

---
+++ word-ladder/word-ladder.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    using WordSetT = std::unordered_set<std::string>;
+    using TransitionMapT = std::unordered_map<std::string, WordSetT>;
+
+    static auto createKey(const std::string& word, int i) noexcept {
+        return word.substr(0, i) + "*" + word.substr(i+1);
+    }
+
+    static TransitionMapT createTransitionMap(const std::vector<std::string>& wordList) noexcept {
+        TransitionMapT myTransitionMap;
+        for (const auto& word : wordList) {
+            for (auto i = 0; i < word.size(); ++i) {
+                myTransitionMap[createKey(word, i)].emplace(word);
+            }
+        }
+        return myTransitionMap;
+    }
+public:
+    int ladderLength(const string& beginWord, const string& endWord, const vector<string>& wordList) {
+        std::queue<std::pair<std::string, int>> myQueue;
+        myQueue.push(std::make_pair(beginWord, 1));
+        auto seenWords = WordSetT{beginWord};
+        auto transitionMap = createTransitionMap(wordList);
+        while (!myQueue.empty()) {
+            const auto& [word, level] = myQueue.front();
+            for (auto i = 0; i < word.size(); ++i) {
+                if (word == endWord) {
+                    return level;
+                }
+                auto key = createKey(word, i);
+                for (const auto& nextWord : transitionMap[key]) {
+                    if (seenWords.find(nextWord) != seenWords.cend()) {
+                        continue;
+                    }
+                    myQueue.push(std::make_pair(nextWord, level+1));
+                    seenWords.emplace(nextWord);
+                }
+            }
+            myQueue.pop();
+        }
+        return 0;
+    }
+};
