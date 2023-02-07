94114ecac308aee500ef875a264937cb25ef21ec
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-07 07:47:27 -0600

    Time: 3 ms (76.68%), Space: 7.9 MB (48.3%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,40 +1,23 @@
 class Solution {
-    using PathT = std::vector<int>;
-    std::vector<PathT> backtrack(int index, PathT& path, std::vector<bool>& usedIdxs, const PathT& nums) { 
-        std::vector<PathT> result;
-        if (path.size() == nums.size()) {
-            result.push_back(path);
-            return result;
+    using ResultT = std::vector<std::vector<int>>;
+    ResultT theResult;
+    void backtrack(int startIndex, std::string path) { 
+        if (startIndex == path.size()) {
+            theResult.emplace_back(std::vector<int>(path.cbegin(), path.cend()));
         }
-        for (auto idx = 0; idx < nums.size(); ++idx) {
-            if (usedIdxs[idx]) {
-                continue;
-            }
-            usedIdxs[idx] = true;
-            path.push_back(nums[idx]);
-            const auto& subResult {backtrack(idx, path, usedIdxs, nums)};
-            std::move(subResult.cbegin(), subResult.cend(), std::back_inserter(result));
-            path.pop_back();
-            usedIdxs[idx] = false;
+        for (auto i = startIndex; i < path.size(); ++i) {
+            std::swap(path[i], path[startIndex]);
+            backtrack(startIndex+1, path);
+            std::swap(path[i], path[startIndex]);
         }
-        return result;
     }
 
 public:
-    vector<PathT> permute(const PathT& nums) {
-        std::vector<PathT> result;
+    ResultT permute(const std::vector<int>& nums) {
         if (nums.empty()) {
-            return result;
+            return theResult;
         }
-        std::vector<bool> usedIdxs(nums.size());
-        for (auto idx = 0; idx < nums.size(); ++idx) {
-            usedIdxs[idx] = true;
-            auto myPath{PathT{nums[idx]}};
-            myPath.reserve(nums.size());
-            const auto& subResult = backtrack(idx, myPath, usedIdxs, nums);
-            std::move(subResult.cbegin(), subResult.cend(), std::back_inserter(result));
-            usedIdxs[idx] = false;
-        }
-        return result;
+        backtrack(0, std::string(nums.cbegin(), nums.cend()));
+        return theResult;
     }
 };
