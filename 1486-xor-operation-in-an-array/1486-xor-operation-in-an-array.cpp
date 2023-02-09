7eaece7aeaeb522c65e691274555962eff4216f5
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-09 15:58:22 -0600

    Time: 3 ms (40.88%), Space: 6 MB (70.34%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,7 +1,7 @@
 
 class Solution {
 public:
-    int xorOperation(int n, int start) {
+    constexpr int xorOperation(const int n, const int start) const {
         auto result = start;
         for (auto i = 1; i < n; ++i) {
             result ^= start + 2*i;
