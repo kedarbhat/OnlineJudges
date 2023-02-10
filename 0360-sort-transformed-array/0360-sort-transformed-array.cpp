c485ec433d746377a7309fdcb70f227208814c86
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-10 17:07:47 -0600

    Time: 2 ms (85.48%), Space: 9.3 MB (50.81%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.cpp
+++ 0360-sort-transformed-array/0360-sort-transformed-array.cpp
@@ -1,12 +1,38 @@
 class Solution {
+    static constexpr auto Calculate = [](int a, int b, int c) {
+        return [=](int x) { return a*x*x + b*x + c; };
+    };
 public:
     vector<int> sortTransformedArray(const vector<int>& nums, int a, int b, int c) {
-        static constexpr auto Calculate = [](int a, int b, int c) {
-            return [=](int x) { return a*x*x + b*x + c; };
-        };
+        auto myCalculator = Calculate(a, b, c);
         std::vector<int> result;
-        std::transform(nums.cbegin(), nums.cend(), std::back_inserter(result), Calculate(a, b, c));
-        std::sort(result.begin(), result.end());
+        result.reserve(nums.size());
+        int left = 0;
+        int right = nums.size()-1;
+        while (left <= right) {
+            auto leftResult = myCalculator(nums[left]);
+            auto rightResult = myCalculator(nums[right]);
+            if (a < 0) {
+                if (leftResult < rightResult) {
+                    result.emplace_back(leftResult);
+                    ++left;
+                } else {
+                    result.emplace_back(rightResult);
+                    --right;
+                }
+            } else {
+                 if (leftResult > rightResult) {
+                    result.emplace_back(leftResult);
+                    ++left;
+                } else {
+                    result.emplace_back(rightResult);
+                    --right;
+                }
+            }
+        }
+        if (a >= 0) {
+            std::reverse(result.begin(), result.end());
+        }
         return result;
     }
 };
