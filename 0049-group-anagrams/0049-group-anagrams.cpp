0b4010a2c8d942977ff27bba032e47f342512d3d
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-11 16:07:00 -0600

    Time: 47 ms (45.51%), Space: 20.2 MB (69.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,3 +1,15 @@
+template<typename ValueType, std::size_t N>
+struct std::hash<std::array<ValueType, N>> {
+    constexpr std::size_t operator()(const std::array<ValueType, N>& anArray) const noexcept {
+        auto myHasher = std::hash<ValueType>{};
+        std::size_t hashResult = 0;
+        for (auto i : anArray) {
+            hashResult = (hashResult << 5) + myHasher(i);
+        }
+        return hashResult;
+    }
+};
+
 class Solution {
     using RepeatedLetterCountsT = std::array<int, 26u>;
     static constexpr RepeatedLetterCountsT createRepeatedLetterCounts(std::string_view str) noexcept {
@@ -9,27 +21,18 @@ class Solution {
         return myRepeatedLetterCounts;
     }
 
-    std::string createGroupedAnagram(std::string_view str) const noexcept {                
-        std::string myGroupedAnagram;
-        myGroupedAnagram.reserve(str.size());
-        const auto& myRepeatedLetterCounts{createRepeatedLetterCounts(str)};
-        for (auto charIdx = 0u; charIdx < myRepeatedLetterCounts.size(); ++charIdx) {
-            myGroupedAnagram.append(myRepeatedLetterCounts[charIdx], charIdx+'a');            
-        }
-        return myGroupedAnagram;
-    }
-
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        std::unordered_map<std::string, std::vector<std::string>> myGroupedAnagrams;
+        std::unordered_map<RepeatedLetterCountsT, std::vector<std::string>> myGroupedAnagrams;
         for (const auto& str : strs) {
-            myGroupedAnagrams[createGroupedAnagram(str)].push_back(str);
+            myGroupedAnagrams[createRepeatedLetterCounts(str)].push_back(str);
         }
         std::vector<std::vector<std::string>> myResult;
         std::transform(
             std::make_move_iterator(myGroupedAnagrams.begin()), 
             std::make_move_iterator(myGroupedAnagrams.end()), 
-            std::back_inserter(myResult), [](auto&& p) { return p.second; });       
+            std::back_inserter(myResult), 
+            [](auto&& p) { return std::move(p.second); });       
         return myResult;
     }
 };
