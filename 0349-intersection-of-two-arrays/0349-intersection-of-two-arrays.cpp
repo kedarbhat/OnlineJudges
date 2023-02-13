dc2e85a7882d737f6d44ba97eff778e55a6dcbf2
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-12 20:10:13 -0600

    Time: 7 ms (69.28%), Space: 10.5 MB (27.95%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     std::vector<int> intersection(const std::vector<int>& nums1, const std::vector<int>& nums2) noexcept {
-        std::unordered_set numsSet1(nums1.cbegin(), nums1.cend());
-        std::unordered_set numsSet2(nums2.cbegin(), nums2.cend());
-        std::vector<int> result;
-        for (auto i = 0; i < 1001; ++i) {
-            if (numsSet1.find(i) != numsSet1.cend() && numsSet2.find(i) != numsSet2.cend()) {
-                result.emplace_back(i);
+        if (nums1 > nums2) {
+            return intersection(nums2, nums1);
+        }
+        std::bitset<1001> alreadySeen;
+        std::unordered_set<int> numsSet2(nums2.cbegin(), nums2.cend());
+        std::vector<int> myIntersection;
+        for (auto num : nums1) {
+            if (!alreadySeen[num] && numsSet2.find(num) != numsSet2.cend()) {
+                myIntersection.emplace_back(num);
+                alreadySeen[num] = true;
             }
         }
-        return result;
+        return myIntersection;
     }
 };
