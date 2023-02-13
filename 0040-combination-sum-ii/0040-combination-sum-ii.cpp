8d7429eecfb03987bcd0411ae76e134026bcbe68
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-12 22:32:19 -0600

    Time: 7 ms (64.65%), Space: 10.9 MB (32.90%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    std::vector<std::vector<int>> theResult;
+    std::vector<int> theCandidates;
+    int theTarget;
+    void dfs(int startIdx, std::vector<int>& currentPath, int currentSum) noexcept {
+        if (currentSum > theTarget) {
+            return;
+        }
+        if (currentSum == theTarget) {
+            theResult.emplace_back(currentPath);
+            return;
+        }
+        for (int i = startIdx; i < theCandidates.size(); ++i) {
+            if (i > startIdx && theCandidates[i] == theCandidates[i-1]) {
+                continue;
+            }
+            currentPath.emplace_back(theCandidates[i]);
+            dfs(i+1, currentPath, currentSum + theCandidates[i]);
+            currentPath.pop_back();
+        }
+    }
+public:
+    std::vector<std::vector<int>> combinationSum2(std::vector<int>& candidates, int target) noexcept {
+        std::move(candidates.begin(), candidates.end(), std::back_inserter(theCandidates));
+        std::sort(theCandidates.begin(), theCandidates.end());
+        theTarget = target;
+        std::vector<int> path;
+        dfs(0, path, 0);
+        return theResult;
+    }
+};
