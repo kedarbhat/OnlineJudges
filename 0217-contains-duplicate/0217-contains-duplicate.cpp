53f4ed16d70cd19707bea6a82d3972033857fe1a
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-12 19:38:44 -0600

    Time: 124 ms (63.11%), Space: 69.8 MB (18.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    bool containsDuplicate(const vector<int>& nums) {
+    bool containsDuplicate(const vector<int>& nums) noexcept {
         std::unordered_set<int> mySeenNums(nums.size());
         for (auto i : nums) {
-            const auto& result = mySeenNums.emplace(i);
-            if (!result.second) {
+            if (!mySeenNums.emplace(i).second) {
                 return true;
             }
         }
