78739ae98e73672a3e7cc9c8d9942587fc4f6de0
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-13 17:57:54 -0600

    Time: 25 ms (51.48%), Space: 10.6 MB (33.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(std::string_view s) noexcept {
-       auto longestSubstring = 0;
-       std::bitset<256> charsUsed;
-       auto slow = 0;
-       for (auto fast = 0; fast < std::size(s); ++fast) {
-           while (charsUsed[s[fast]]) {
-               charsUsed[s[slow]] = false;
-               ++slow;
-           }
-           charsUsed[s[fast]] = true;
-           longestSubstring = std::max(longestSubstring, fast-slow+1);
-       }
-       return longestSubstring;
+        if (s.empty()) {
+            return 0;
+        }
+        int longestSubstring = 0;
+        std::unordered_map<char, int> usedLetters;
+        for (int i = 0, sequenceStart = 0; i < s.size(); ++i) {
+            auto current = s[i];
+            if (usedLetters.find(current) == usedLetters.cend()) {
+                usedLetters[current] = i;
+                longestSubstring = std::max(longestSubstring, static_cast<int>(usedLetters.size()));
+            } else {
+                longestSubstring = std::max(longestSubstring, static_cast<int>(usedLetters.size()));
+                const auto finish = usedLetters[current];
+                for (auto j = sequenceStart; j <= finish; ++j) {
+                    usedLetters.erase(s[j]);
+                }
+                usedLetters[current] = i;
+                sequenceStart = finish+1;
+            }
+        }
+        return longestSubstring;
     }
 };
