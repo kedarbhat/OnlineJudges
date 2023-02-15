c92fc77016968cc0a7a4aa87792a6ea6de474805
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-15 15:18:34 -0600

    Time: 7 ms (62.57%), Space: 10.7 MB (24.44%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
+    std::unordered_map<int, int> createCounter(const std::vector<int>& nums) noexcept {
+        std::unordered_map<int, int> myCounter;
+        for (auto num : nums) {
+            ++myCounter[num];
+        }
+        return myCounter;
+    }
 public:
     vector<int> intersect(const vector<int>& nums1, const vector<int>& nums2) noexcept {    
-        std::unordered_map<int, int> myNums1Counter;
-        for (auto num : nums1) {
-            ++myNums1Counter[num];
-        }
-        std::unordered_map<int, int> myNums2Counter;
-        for (auto num : nums2) {
-            ++myNums2Counter[num];
-        }
+        auto myFirstCounter{createCounter(nums1)};
+        auto mysecondCounter{createCounter(nums2)};
         std::vector<int> result;
         for (auto num : nums2) {
-            if (auto iter = myNums1Counter.find(num); iter != myNums1Counter.cend()) {
-                auto numRepeats = std::min(myNums1Counter[num], myNums2Counter[num]);
+            if (auto iter = myFirstCounter.find(num); iter != myFirstCounter.cend()) {
+                auto numRepeats = std::min(myFirstCounter[num], mysecondCounter[num]);
                 std::fill_n(std::back_inserter(result), numRepeats, num);
-                myNums1Counter.erase(num);
+                myFirstCounter.erase(num);
             }
         }
         return result;
