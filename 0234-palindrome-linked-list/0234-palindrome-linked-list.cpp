69033d0377f3fae03351f11890c457449f02afa6
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-15 22:14:20 -0600

    Time: 250 ms (67.49%), Space: 114.3 MB (73.46%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,25 +9,40 @@
  * };
  */
 class Solution {
+    static constexpr ListNode* reverseList(ListNode* aNode) noexcept {
+        ListNode* myPrev = nullptr;
+        ListNode* myCurrent = aNode;
+        ListNode* myNext = nullptr;
+        while (myCurrent != nullptr) {
+            myNext = myCurrent->next;
+            myCurrent->next = myPrev;
+            myPrev = myCurrent;
+            myCurrent = myNext;
+        }
+        return myPrev;
+    }
 public:
-    bool isPalindrome(ListNode* head) noexcept {
+    static constexpr bool isPalindrome(ListNode* head) noexcept {
         if (head == nullptr) {
             return true;
         }
-        std::vector<int> theListNodeValues;
         auto* myPtr = head;
-        while (myPtr != nullptr) {
-            theListNodeValues.push_back(myPtr->val);
+        auto* myPtr2 = head;
+        while (myPtr2 != nullptr && myPtr2->next != nullptr) {
+            myPtr = myPtr->next;
+            myPtr2 = myPtr2->next->next;
+        }
+        if (myPtr2 != nullptr) {
             myPtr = myPtr->next;
         }
-        auto myFirst = theListNodeValues.cbegin();
-        auto myLast = theListNodeValues.cend() - 1;
-        while (myFirst < myLast) {
-            if (*myFirst != *myLast) {
+        myPtr = reverseList(myPtr);
+        myPtr2 = head;
+        while (myPtr != nullptr) {
+            if (myPtr->val != myPtr2->val) {
                 return false;
             }
-            ++myFirst;
-            --myLast;
+            myPtr = myPtr->next;
+            myPtr2 = myPtr2->next;
         }
         return true;
     }
