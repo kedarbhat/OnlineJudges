4d2bb43c1d10902c30e0ac95361d4546ba89c0f4
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-16 15:19:28 -0600

    Time: 7 ms (77.23%), Space: 13 MB (85.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int orangesRotting(std::vector<std::vector<int>>& grid) {
+int orangesRotting(std::vector<std::vector<int>>& grid) noexcept {
     int rows = grid.size();
     int cols = grid[0].size();
     int minutes = 0;
@@ -10,7 +10,7 @@ int orangesRotting(std::vector<std::vector<int>>& grid) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             if (grid[i][j] == 1) {
-                freshOranges++;
+                ++freshOranges;
             } else if (grid[i][j] == 2) {
                 rotten.push(std::make_pair(i, j));
             }
