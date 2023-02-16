8fac15253438286ed1906892abf565b5b59e22b5
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-15 22:36:43 -0600

    Time: 183 ms (25.61%), Space: 33.9 MB (82.65%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,14 +1,16 @@
 class Solution {
-  std::unordered_map<std::string, std::string> emailToName;
-  std::unordered_map<std::string, std::string> emailToParent;
- public:
-  std::string findParent(const std::string &aChild) {
+  std::unordered_map<std::string_view, std::string_view> emailToName;
+  std::unordered_map<std::string_view, std::string_view> emailToParent;
+
+  std::string_view findParent(std::string_view aChild) noexcept {
     if (emailToParent[aChild] == aChild) {
-      return aChild;
+        return aChild;
     }
-    return emailToParent[aChild] = findParent(emailToParent[aChild]);
+    emailToParent[aChild] = findParent(emailToParent[aChild]);
+    return emailToParent[aChild];
   }
-  std::vector<std::vector<std::string>> accountsMerge(std::vector<std::vector<std::string>>& accounts) {
+ public:  
+  std::vector<std::vector<std::string>> accountsMerge(std::vector<std::vector<std::string>>& accounts) noexcept {
     std::vector<std::vector<std::string>> result;
     for (const auto& account : accounts) {
       const auto& name = account[0];
@@ -24,16 +26,16 @@ class Solution {
         emailToParent[findParent(account[i])] = parent;
       }
     }
-    std::unordered_map<std::string, std::set<std::string>> merged;
+    std::unordered_map<std::string_view, std::set<std::string_view>> merged;
     for (const auto& account : accounts) {
       for (auto i = 1u; i < account.size(); ++i) {
         merged[findParent(account[i])].insert(account[i]);
       }
     }
     for (const auto& [parent, emails] : merged) {
-      std::vector<std::string> account{emailToName[parent]};
+      std::vector<std::string> account{std::string{emailToName[parent]}};
       account.insert(account.end(), emails.cbegin(), emails.cend());
-      result.emplace_back(move(account));
+      result.emplace_back(account);
     }
     return result;        
   }
