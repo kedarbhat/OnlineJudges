bd6046b509fb2ec2601282cfd42726c4c6668446
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-16 17:53:15 -0600

    Time: 29 ms (60.22%), Space: 6.8 MB (97.40%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,27 @@
 class Solution {
-    
-    static auto createTable(std::size_t n) noexcept {
-        using TableT = std::vector<std::vector<bool>>;
-        TableT table(n);
-        std::fill(table.begin(), table.end(), std::vector<bool>(n));
-        return table;
+    static constexpr int expandAroundCenter(std::string_view s, int left, int right) noexcept {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 public:
-    string longestPalindrome(string_view s) {
-        auto dpTable = createTable(s.size());
-        auto minStart = 0;
-        auto maxFinish = 0;
-        for (auto finish{0}; finish < s.size(); ++finish) {
-            for (auto start{0}; start < finish+1; ++start) {
-                if (s[start] == s[finish]) {
-                    if ((start <= finish && finish <= start + 1) || dpTable[start+1][finish-1]) {
-                        dpTable[start][finish] = true;
-                        if (finish-start > maxFinish-minStart) {
-                            minStart = start;
-                            maxFinish = finish;
-                        }
-                    }
-                }
+    string longestPalindrome(std::string_view s) noexcept {
+        int n = s.length();
+        if (n == 0) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i+1);
+            int len = max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return std::string(next(s.cbegin(), minStart), next(s.cbegin(), maxFinish+1));
+        return std::string{s.substr(start, end - start + 1)};
     }
 };
