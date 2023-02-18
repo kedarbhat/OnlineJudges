aaf090bc4700d7caa452a707211583db41b5ab97
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-18 00:21:50 -0600

    Time: 202 ms (87.7%), Space: 103.1 MB (33.33%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,25 +1,17 @@
-
 class Solution {
-    std::vector<std::vector<int>> theResult;
-    int theN{};
-    int theK{};
-    void backtrack(int start, std::vector<int>& current) noexcept {
-        if (current.size() == theK) {
-            theResult.emplace_back(current);
-            return;
-        }
-        for (auto j = start; j < theN+1; ++j) {
-            current.push_back(j);
-            backtrack(j+1, current);
-            current.pop_back();
-        }
-    }
 public:
-    std::vector<std::vector<int>> combine(int n, int k) {
-        theN = n;
-        theK = k;
-        auto current = std::vector<int>{};
-        backtrack(1, current);
-        return theResult;
+    vector<int> dailyTemperatures(const vector<int>& temperatures) {
+        std::stack<int> myIndexStack;
+        std::vector<int> result(temperatures.size(), 0);
+        for (auto currentTempIdx = 0; currentTempIdx < temperatures.size(); ++currentTempIdx) {
+            auto currentTemp = temperatures[currentTempIdx];
+            while (!myIndexStack.empty() && temperatures[myIndexStack.top()] < currentTemp) {
+                auto prevDayIdx = myIndexStack.top();
+                myIndexStack.pop();
+                result[prevDayIdx] = currentTempIdx-prevDayIdx;
+            }
+            myIndexStack.push(currentTempIdx);
+        }
+        return result;
     }
 };
