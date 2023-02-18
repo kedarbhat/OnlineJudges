57fd00875bacb91f935bef8dc3e28b6fb247edc8
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-18 01:49:30 -0600

    Time: 61 ms (93.27%), Space: 34.5 MB (76.32%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int leastInterval(std::vector<char>& tasks, int n) const noexcept {
+    int leastInterval(const std::vector<char>& tasks, int n) const noexcept {
         int result = 0;
-        std::vector<int> task_count(26, 0);
+        std::array<int, 26> taskCount{};
         for (auto task : tasks) {
-            task_count[task - 'A']++;
+            ++taskCount[task - 'A'];
         }
-        std::sort(task_count.begin(), task_count.end());
-        int max_val = task_count[25] - 1;
-        int idle_slots = max_val * n;
-        for (int i = 24; i >= 0 && task_count[i] > 0; i--) {
-            idle_slots -= std::min(task_count[i], max_val);
+        std::sort(taskCount.begin(), taskCount.end());
+        int maxVal = taskCount[25] - 1;
+        int idleSlots = maxVal * n;
+        for (int i = 24; i >= 0 && taskCount[i] > 0; --i) {
+            idleSlots -= std::min(taskCount[i], maxVal);
         }
-        return idle_slots > 0 ? idle_slots + tasks.size() : tasks.size();
+        return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
     }        
 };
