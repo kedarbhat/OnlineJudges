36c0e11ef924c1787a686681e26a442f2e3a2cdf
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-18 16:03:45 -0600

    Time: 7 ms (68.3%), Space: 8.8 MB (64.66%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,29 +1,41 @@
-class Solution {
- public:
-  int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-    int n = grid.size();
-    if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
-    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-    queue<pair<int, int>> q;
-    q.push({0, 0});
-    grid[0][0] = 1;
-    int step = 1;
-    while (!q.empty()) {
-      int size = q.size();
-      for (int i = 0; i < size; ++i) {
-        auto [x, y] = q.front();
-        q.pop();
-        if (x == n - 1 && y == n - 1) return step;
-        for (auto& dir : dirs) {
-          int nx = x + dir[0];
-          int ny = y + dir[1];
-          if (nx < 0 || nx >= n || ny < 0 || ny >= n || grid[nx][ny] == 1) continue;
-          q.push({nx, ny});
-          grid[nx][ny] = 1;
-        }
-      }
-      ++step;
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
     }
-    return -1;
-  }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    std::unordered_map<Node*, Node*> clonedNodes;
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) {
+            return node;
+        }
+
+        if (Node* neighbor = clonedNodes[node]; neighbor != nullptr) {
+            return neighbor;
+        }
+
+        Node* newNode = new Node(node->val);
+        clonedNodes[node] = newNode;
+        std::transform(node->neighbors.cbegin(), node->neighbors.cend(), 
+            std::back_inserter(newNode->neighbors), 
+            [&](Node* neighbor) { return cloneGraph(neighbor); });
+        return newNode;
+    }
 };
