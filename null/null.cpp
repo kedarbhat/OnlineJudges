f5f98e577bcb7058cb9f30854fffb6c0c3fa9e67
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-18 14:57:10 -0600

    Time: 171 ms (83.1%), Space: 46.8 MB (98.71%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,20 +1,34 @@
 class Solution {
-public:
-    int furthestBuilding(const std::vector<int>& heights, int bricks, int ladders) noexcept {
-        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-        for (int i = 0; i < heights.size() - 1; ++i) {
-            int diff = heights[i + 1] - heights[i];
-            if (diff > 0) {
-                pq.push(diff);
-                if (pq.size() > ladders) {
-                    bricks -= pq.top();
-                    pq.pop();
-                }
-                if (bricks < 0) {
-                    return i;
-                }
-            }
-        }
-        return heights.size() - 1;
+  int findRoot(std::vector<int>& parent, int i) {
+    while (parent[i] != i) {
+      parent[i] = parent[parent[i]];
+      i = parent[i];
     }
+    return i;
+  }
+ public:
+  std::string smallestStringWithSwaps(std::string aString, const std::vector<std::vector<int>>& pairs) {
+    std::vector<int> parent(aString.size());
+    for (int i = 0; i < parent.size(); ++i) {
+      parent[i] = i;
+    }
+    for (const auto& pair : pairs) {
+      const int root1 = findRoot(parent, pair[0]);
+      const int root2 = findRoot(parent, pair[1]);
+      if (root1 != root2) {
+        parent[root1] = root2;
+      }
+    }
+    std::unordered_map<int, std::priority_queue<char, std::vector<char>, std::greater<>>> map;
+    for (int i = 0; i < aString.size(); ++i) {
+      const int root = findRoot(parent, i);
+      map[root].push(aString[i]);
+    }
+    for (int i = 0; i < aString.size(); ++i) {
+      const int root = findRoot(parent, i);
+      aString[i] = map[root].top();
+      map[root].pop();
+    }
+    return aString;
+  }
 };
