692f9066e9c8700229de6c6184403873c86e8009
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-18 15:43:14 -0600

    Time: 196 ms (93.7%), Space: 78.5 MB (79.60%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -3,7 +3,7 @@ class Solution {
   int carFleet(int target, std::vector<int>& position, std::vector<int>& speed) {
     std::vector<std::pair<int, int>> cars;
     for (int i = 0; i < position.size(); ++i) {
-      cars.push_back(std::make_pair(position[i], speed[i]));
+      cars.emplace_back(position[i], speed[i]);
     }
     std::sort(cars.begin(), cars.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
       return a.first > b.first;
