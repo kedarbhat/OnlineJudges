4346208d063fb504db071f4f0b704bd1cd585bf8
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-17 23:14:54 -0600

    Time: 32 ms (35.18%), Space: 13.8 MB (76.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int findCircleNum(std::vector<std::vector<int>>& isConnected) noexcept {
-        const int n = isConnected.size();
-        std::vector<bool> visited(n, false);
-        int provincesNum = 0;
+        using ssize_t = std::make_signed_t<std::size_t>;
+        auto n = static_cast<ssize_t>(isConnected.size());
+        std::vector<int> parent(n);
         for (int i = 0; i < n; ++i) {
-            if (visited[i]) {
-                continue;
-            }
-            ++provincesNum;
-            std::stack<int> stack;
-            stack.push(i);
-            while (!stack.empty()) {
-                auto j = stack.top();
-                stack.pop();
-                visited[j] = true;
-                for (int k = 0; k < n; ++k) {
-                    if (isConnected[j][k] == 1 && !visited[k]) {
-                        stack.push(k);
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (isConnected[i][j] == 1) {
+                    int p1 = i;
+                    while (p1 != parent[p1]) {
+                        p1 = parent[p1];
                     }
+                    int p2 = j;
+                    while (p2 != parent[p2]) {
+                        p2 = parent[p2];
+                    }
+                    parent[p2] = p1;
                 }
             }
         }
-        return provincesNum;
+
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            if (parent[i] == i) {
+                ++count;
+            }
+        }
+        return count;
     }
 };
