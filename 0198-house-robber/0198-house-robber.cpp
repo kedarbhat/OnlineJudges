26cb14b98e538a68d20ffc64127015434d52a5d6
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2023-02-18 01:10:11 -0600

    Time: 6 ms (8.68%), Space: 7.9 MB (34.66%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(const std::vector<int>& nums) noexcept {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        std::vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        dp[1] = std::max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); ++i) {
+            dp[i] = std::max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp.back(); 
+    }
+};
